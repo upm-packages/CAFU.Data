2d6d3db790c2b5d3a6285c4a053790e9d16b2867
Tetsuya Mori <monry84@gmail.com>
2020-05-18 14:43:41 +0900

    :sparkles: [Destructive] Support to switch binding by SynchronizeMode

--- Assets/Scripts/Installer/DataAdapterInstaller.cs
+++ Assets/Scripts/Installer/DataAdapterInstaller.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using CAFU.Data.DataSerializer;
 using CAFU.Data.DataSource;
 using CAFU.Data.Repository;
@@ -6,58 +7,85 @@ using Zenject;
 
 namespace CAFU.Data.Installer
 {
-    public class DataAdapterInstaller<T> : Installer<DataSourceType, DataAdapterInstaller<T>>
+    public sealed class DataAdapterInstaller<T> : Installer<DataSourceType, SynchronizeMode, DataAdapterInstaller<T>>
         where T : new()
     {
         private readonly DataSourceType dataSourceType;
+        private readonly SynchronizeMode synchronizeMode;
 
-        public DataAdapterInstaller(DataSourceType dataSourceType)
+        public DataAdapterInstaller(DataSourceType dataSourceType, SynchronizeMode synchronizeMode)
         {
             this.dataSourceType = dataSourceType;
+            this.synchronizeMode = synchronizeMode;
         }
 
         public override void InstallBindings()
         {
-            DataAdapterInstaller<T, JsonUtility<T>>.Install(Container, dataSourceType, JsonUtility<T>.Default);
+            DataAdapterInstaller<T, JsonUtility<T>>.Install(Container, dataSourceType, synchronizeMode, JsonUtility<T>.Default);
         }
     }
 
-    public class DataAdapterInstaller<T, TDataSerializer> : Installer<DataSourceType, TDataSerializer, DataAdapterInstaller<T, TDataSerializer>>
+    public sealed class DataAdapterInstaller<T, TDataSerializer> : Installer<DataSourceType, SynchronizeMode, TDataSerializer, DataAdapterInstaller<T, TDataSerializer>>
         where T : new()
         where TDataSerializer : IDataSerializer<T>
     {
         private readonly DataSourceType dataSourceType;
+        private readonly SynchronizeMode synchronizeMode;
         private readonly TDataSerializer dataSerializer;
 
-        public DataAdapterInstaller(DataSourceType dataSourceType, TDataSerializer dataSerializer)
+        public DataAdapterInstaller(DataSourceType dataSourceType, SynchronizeMode synchronizeMode, TDataSerializer dataSerializer)
         {
             this.dataSourceType = dataSourceType;
+            this.synchronizeMode = synchronizeMode;
             this.dataSerializer = dataSerializer;
         }
 
         public override void InstallBindings()
         {
+            if (!TypeMap.Interface.ContainsKey(synchronizeMode))
+            {
+                throw new ArgumentException($"Interface for '{synchronizeMode}' does not defined.");
+            }
+            if (!TypeMap.Repository.ContainsKey(synchronizeMode))
+            {
+                throw new ArgumentException($"Repository for '{synchronizeMode}' does not defined.");
+            }
+            if (!TypeMap.DataSource.ContainsKey((dataSourceType, synchronizeMode)))
+            {
+                throw new ArgumentException($"DataSource type '{dataSourceType}' for '{synchronizeMode}' does not defined.");
+            }
+
             Container
-                .Bind<IAsyncDataAdapter<T>>()
+                .Bind(TypeMap.Interface[synchronizeMode].MakeGenericType(typeof(T)))
                 .FromSubContainerResolve()
                 .ByMethod(
                     subContainer =>
                     {
-                        subContainer.BindInterfacesTo<AsyncDataRepository<T>>().AsSingle();
                         subContainer.Bind<IDataSerializer<T>>().FromInstance(dataSerializer).AsSingle();
-                        switch (dataSourceType)
-                        {
-                            case DataSourceType.FileSystem:
-                                subContainer.BindInterfacesTo<FileSystem>().AsSingle();
-                                break;
-                            case DataSourceType.WebRequest:
-                                subContainer.BindInterfacesTo<WebRequest>().AsSingle();
-                                break;
-                            default:
-                                throw new ArgumentOutOfRangeException();
-                        }
+                        subContainer.BindInterfacesTo(TypeMap.Repository[synchronizeMode].MakeGenericType(typeof(T))).AsSingle();
+                        subContainer.BindInterfacesTo(TypeMap.DataSource[(dataSourceType, synchronizeMode)]).AsSingle();
                     }
                 ).AsSingle();
         }
+    }
+
+    internal static class TypeMap
+    {
+        internal static readonly IDictionary<SynchronizeMode, Type> Interface = new Dictionary<SynchronizeMode, Type>
+        {
+            {SynchronizeMode.Sync, typeof(IDataAdapter<>)},
+            {SynchronizeMode.Async, typeof(IAsyncDataAdapter<>)},
+        };
+        internal static readonly IDictionary<SynchronizeMode, Type> Repository = new Dictionary<SynchronizeMode, Type>
+        {
+            {SynchronizeMode.Sync, typeof(DataRepository<>)},
+            {SynchronizeMode.Async, typeof(AsyncDataRepository<>)},
+        };
+        internal static readonly IDictionary<(DataSourceType, SynchronizeMode), Type> DataSource = new Dictionary<(DataSourceType, SynchronizeMode), Type>
+        {
+            {(DataSourceType.FileSystem, SynchronizeMode.Sync), typeof(FileSystem)},
+            {(DataSourceType.FileSystem, SynchronizeMode.Async), typeof(AsyncFileSystem)},
+            {(DataSourceType.WebRequest, SynchronizeMode.Async), typeof(AsyncWebRequest)},
+        };
     }
 }
